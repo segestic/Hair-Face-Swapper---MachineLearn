378a54ff9f151725b114b4637fb7103dd363ee9a
olusegun <olusegunodewole1@gmail.com>
2022-04-20 14:26:10 +0300

    model-length increase

--- pages/models.py
+++ pages/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=120, null=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
-    style = models.ImageField(null=True)
-    pic = models.ImageField(null=True)
-    merge = models.ImageField(null=True)
+    style = models.ImageField(max_length=255, null=True)
+    pic = models.ImageField(max_length=255, null=True)
+    merge = models.ImageField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
    
